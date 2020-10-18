808c561daa9bf1fdd1ae3b15d5f4b3fa8f50f94d
tobesh01 <animashauntaofiq@yahoo.com>
2020-10-18 16:48:48 +0100

    Moved StateInfo to branch 03

--- main.js
+++ main.js
@@ -1,57 +1,62 @@
-// Set Variables
-const select = document.querySelector('select');
 const input = document.querySelector('#input');
-const output = document.querySelector('#output');
-const gramsOutput = document.getElementById('gramsOutput');
-const kgOutput = document.getElementById('kgOutput');
-const ozOutput = document.getElementById('ozOutput');
-const gramsBlock = document.getElementById('grams-block');
-const kgBlock = document.getElementById('kilograms-block');
-const ozBlock = document.getElementById('ounce-block');
+const resultList = document.querySelector('.result-list');
+const mainPalette = document.querySelector('.plus');
+const tabs = document.querySelector('.tabs');
+const mainTab = document.querySelector('.about-contact');
+const toggle = document.querySelector('#toggle')
 
-// Event Listeners
-select.addEventListener('click', selectChange);
-input.addEventListener('input', getInput);
-
-// Hide Output
-output.style.visibility = 'hidden';
-
-// Function Call
-function getInput() {
-    output.style.visibility = 'visible';
-    const inputValue = input.value;
-    gramsOutput.innerHTML = inputValue / 0.0022046;
-    kgOutput.innerHTML = inputValue / 2.2046;
-    ozOutput.innerHTML = inputValue * 16;
-}
-
-function selectChange() {
-    const inputValue = input.value;
-    output.style.visibility = 'visible';
-    if (select.value == 'grams') {
-        gramsBlock.style.display = 'none';
-        kgBlock.style.display = 'block';
-        ozBlock.style.display = 'block';
-        kgOutput.innerHTML = inputValue / 2.2046;
-        ozOutput.innerHTML = inputValue * 16;
-    } else if (select.value == 'kilograms') {
-        kgBlock.style.display = 'none';
-        gramsBlock.style.display = 'block';
-        ozBlock.style.display = 'block';
-        gramsOutput.innerHTML = inputValue / 0.0022046;
-        ozOutput.innerHTML = inputValue * 16;
-    } else if (select.value == 'ounce') {
-        ozBlock.style.display = 'none';
-        kgBlock.style.display = 'block';
-        gramsBlock.style.display = 'block';
-        gramsOutput.innerHTML = inputValue / 0.0022046;
-        kgOutput.innerHTML = inputValue / 2.2046;
-    } else {
-        ozBlock.style.display = 'block';
-        gramsBlock.style.display = 'block';
-        kgBlock.style.display = 'block';
-        gramsOutput.innerHTML = inputValue / 0.0022046;
-        kgOutput.innerHTML = inputValue / 2.2046;
-        ozOutput.innerHTML = inputValue * 16;
+// Activating main Palette
+mainTab.addEventListener('click', (e) => {
+    mainPalette.classList.toggle('activate');
+    tabs.classList.toggle('tab-active');
+});
+// Tabs
+tabs.addEventListener('click', (e) => {
+    if (e.target.classList.contains('c1') || e.target.classList.contains('c1-img') || e.target.classList.contains('c1-span')) {
+        contactTab.style.display = 'block';
+    } else if (e.target.classList.contains('c2') || e.target.classList.contains('c2-img') || e.target.classList.contains('c2-span')) {
+        contactTab.style.display = 'block';
     }
-}
+});
+// Background Toggle
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('light')
+});
+
+// Search data.json and result it
+const inputData = async inputText => {
+    const res = await fetch('./stateCapital.json');
+    const data = await res.json();
+
+    // Get matches to current text input
+    let matches = data.filter(data => {
+        const regex = new RegExp(`^${inputText}`, 'gi');
+        return data.name.match(regex) || data.abbr.match(regex);
+    });
+
+    if (inputText.length === 0) {
+        matches = [];
+        resultList.innerHTML = '';
+    }
+
+    outputHtml(matches);
+};
+
+// Show results in HTML
+const outputHtml = matches => {
+    if (matches.length > 0) {
+        const result = matches.map(match => `
+        <div class="result--">
+        <div>
+        <h4>${match.name} (${match.abbr}) <span class="cap">${match.capital}</span></h4>
+        <p><strong>Slogan:</strong> ${match.title}</p>
+        <p><strong>Postal Code:</strong> ${match.postal}</p>
+        </div>
+        <hr class="hide-on-lg">
+        <small><strong>Latitude:</strong> ${match.lat}, <strong>Longitude:</strong> ${match.lng}</small>
+        </div>`).join('');
+        resultList.innerHTML = result;
+    }
+};
+
+input.addEventListener('input', () => inputData(input.value));
